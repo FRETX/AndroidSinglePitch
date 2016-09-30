9e43f2b8a6c26207db0080f7162dbf55c83361dc
babua <onurbabacan@gmail.com>
2016-09-30 12:47:24 +0300

    added guitar string autodetection

--- app/src/main/java/com/fretx/singlepitchdetector/MainActivity.java
+++ app/src/main/java/com/fretx/singlepitchdetector/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     public AudioInputHandler audioInputHandler;
@@ -22,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView pitchText;
     private TextView medianText;
     private PitchView pitchView;
+    private TextView targetPitch;
+    private boolean autoDetectEnabled;
+    float [] tuning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +34,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //UI references
-        pitchText = (TextView) findViewById(R.id.pitchText );
-        medianText = (TextView) findViewById(R.id.medianPitch);
-        pitchView = (PitchView) findViewById(R.id.pitchView);
         initGui();
+        autoDetectEnabled = true;
+
+        final float [] tuning = {82.41f,110.00f,146.83f,196.00f,246.94f,329.63f};
 
         //Audio Parameters
         int maxFs = AudioInputHandler.getMaxSamplingFrequency();
@@ -77,6 +82,18 @@ public class MainActivity extends AppCompatActivity {
                                 } else{
                                     pitchText.setText( "Pitch: " + Integer.toString((int) Math.round(pitch)) + " Hz");
                                     if(yin.medianPitch > 0){
+
+                                        if(autoDetectEnabled){
+                                            float [] differences = tuning.clone();
+                                            for (int i = 0; i < differences.length; i++) {
+                                                differences[i] -= yin.medianPitch;
+                                                differences[i] = Math.abs(differences[i]);
+                                            }
+                                            int minIndex = getMinIndex(differences);
+                                            pitchView.setCenterPitch(tuning[minIndex]);
+                                            targetPitch.setText("Target pitch: " + Float.toString(tuning[minIndex]));
+                                        }
+
                                         medianText.setText( "Median: " + Integer.toString((int) Math.round(yin.medianPitch)) + " Hz");
                                     }
                                 }
@@ -112,6 +129,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initGui(){
+        pitchText = (TextView) findViewById(R.id.pitchText );
+        medianText = (TextView) findViewById(R.id.medianPitch);
+        pitchView = (PitchView) findViewById(R.id.pitchView);
+        targetPitch = (TextView) findViewById(R.id.targetPitchText);
+        Button autoDetectButton = (Button) findViewById(R.id.autoDetectButton);
+        autoDetectButton.setOnClickListener(tunerListener);
         Button eButton = (Button) findViewById(R.id.e_button);
         eButton.setOnClickListener(tunerListener);
         Button aButton = (Button) findViewById(R.id.a_button);
@@ -128,32 +151,52 @@ public class MainActivity extends AppCompatActivity {
 
     private View.OnClickListener tunerListener = new View.OnClickListener() {
         public void onClick(View v) {
-            TextView targetPitch = (TextView) findViewById(R.id.targetPitchText);
             float note = 0;
             switch (v.getId()) {
+                case R.id.autoDetectButton:
+                    MainActivity.this.autoDetectEnabled = true;
+                    break;
                 case R.id.e_button:
-                    note = 82.41f;
+                    note = MainActivity.this.tuning[0];
+                    MainActivity.this.autoDetectEnabled = false;
                     break;
                 case R.id.a_button:
-                    note = 110.00f;
+                    note = MainActivity.this.tuning[1];
+                    MainActivity.this.autoDetectEnabled = false;
                     break;
                 case R.id.d_button:
-                    note = 146.83f;
+                    note = MainActivity.this.tuning[2];
+                    MainActivity.this.autoDetectEnabled = false;
                     break;
                 case R.id.g_button:
-                    note = 196.00f;
+                    note = MainActivity.this.tuning[3];
+                    MainActivity.this.autoDetectEnabled = false;
                     break;
                 case R.id.b_button:
-                    note = 246.94f;
+                    note = MainActivity.this.tuning[4];
+                    MainActivity.this.autoDetectEnabled = false;
                     break;
                 case R.id.ee_button:
-                    note = 329.63f;
+                    note = MainActivity.this.tuning[5];
+                    MainActivity.this.autoDetectEnabled = false;
                     break;
             }
             pitchView.setCenterPitch(note);
             targetPitch.setText("Target pitch: " + Float.toString(note));
         }
     };
+
+    private static int getMinIndex(float[] array) {
+        float minValue = Float.MAX_VALUE;
+        int minIndex = -1;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
 
 }
 
